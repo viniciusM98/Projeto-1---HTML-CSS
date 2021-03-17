e56571170acf0de0a072182e1f026bd96b87ebfe
Vinícius Medeiros da Silva <vinimed.98@gmail.com>
2021-03-17 17:14:29 -0300

    Add a modal for CEP API

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 let button = document.querySelector('#search-btn')
+let buttonClose = document.querySelector('#close-button')
 let zipCodeField = document.querySelector('#input-zip')
-let content = document.querySelector('#zip-code')
+let content = document.querySelector('#zip-code-modal .modal-main')
+let title = document.querySelector('#zip-code-modal .modal-title')
 
-button.addEventListener('click', adicionaDados)
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-function adicionaDados(event) { 
-  event.preventDefault();
-  
+openModalButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.zip-code-modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.zip-code-modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  adicionaDados()
+  if(modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if(modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+function adicionaDados() { 
   let zipCode = zipCodeField.value
+  let zip = document.createElement('h1')
+  let txt = document.createTextNode(zipCode)
 
   zipCode = zipCode.replace(' ', '')
   zipCode = zipCode.replace('.', '')
   zipCode = zipCode.trim()
+
+  title.innerHTML = ''
+  zip.appendChild(txt)
+  title.appendChild(zip)
 
   axios.get(`https://viacep.com.br/ws/${zipCode}/json/`)
     .then(res => {

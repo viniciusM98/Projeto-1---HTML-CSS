70f68dfa03886b09108c54ef6479bdc7701a0e3c
Vinícius Medeiros da Silva <vinimed.98@gmail.com>
2021-03-17 15:30:05 -0300

    Add axios script and app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let button = document.querySelector('#search-btn')
+let zipCodeField = document.querySelector('#input-zip')
+let content = document.querySelector('#zip-code')
+
+button.addEventListener('click', adicionaDados)
+
+function adicionaDados(event) { 
+  event.preventDefault();
+  
+  let zipCode = zipCodeField.value
+
+  axios.get(`https://viacep.com.br/ws/${zipCode}/json/`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}

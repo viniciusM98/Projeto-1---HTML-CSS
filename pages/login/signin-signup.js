c0779c51edb485c7675192618253a6eac541d916
carlosdigital <carlos.goncalves@digitalsys.com.br>
2021-03-25 11:47:35 -0300

    Tela de login com o LocalStorage implementada, semtodos os requisitos

---
+++ pages/login/signin-signup.js
@@ -0,0 +1,33 @@
+var username = document.querySelector('#username-su')
+var email = document.querySelector('#email-su')
+var password = document.querySelector('#password-su')
+var button_register = document.querySelector('#button-su')
+var button_login = document.querySelector('#button-login')
+
+button_register.addEventListener('click', () => {
+  localStorage.setItem('username', username.value)
+  localStorage.setItem('email', email.value)
+  localStorage.setItem('password', password.value)
+})
+
+button_login.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  var usern = document.querySelector('#user-login')
+  var passw = document.querySelector('#password-login')
+
+  var storedUsername = localStorage.getItem('username')
+  var storedPassword = localStorage.getItem('password')
+
+  console.log(typeof(usern.value))
+  console.log(passw.value)
+  console.log(storedUsername)
+  console.log(storedPassword)
+
+  if(usern.value == storedUsername && passw.value == storedPassword) {
+    alert('You are loged in')
+    window.location.href = "https://materializecopy.herokuapp.com/"
+  } else{
+    alert('ERROR.')
+  }
+})

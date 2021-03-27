71d90d43b26138d899b6843acdd0d408fedc4924
Vinícius Medeiros da Silva <vinimed.98@gmail.com>
2021-03-27 17:59:01 -0300

    adjusts

--- login.js
+++ login.js
@@ -7,10 +7,10 @@ sign_up_btn.addEventListener('click', () => {
   container.classList.add('sign-up-mode')
 })
 
-sign_up.addEventListener('click', () => {
+/* sign_up.addEventListener('click', () => {
   container.classList.remove('sign-up-mode')
-})
+}) */
 
-sign_in_btn.addEventListener('click', () => {
+/* sign_in_btn.addEventListener('click', () => {
   container.classList.remove('sign-up-mode')
-})
+}) */
--- signin-signup.js
+++ signin-signup.js
@@ -4,10 +4,17 @@ var password = document.querySelector('#password-su')
 var button_register = document.querySelector('#button-su')
 var button_login = document.querySelector('#button-login')
 
-button_register.addEventListener('click', () => {
-  localStorage.setItem('username', username.value)
-  localStorage.setItem('email', email.value)
-  localStorage.setItem('password', password.value)
+button_register.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  if(username.value.length > 3 && email.value.length > 3 && password.value.length > 3){
+    localStorage.setItem('username', username.value)
+    localStorage.setItem('email', email.value)
+    localStorage.setItem('password', password.value)
+    container.classList.remove('sign-up-mode')
+  } else{
+    alert('ERRO')
+  }
 })
 
 button_login.addEventListener('click', (event) => {
@@ -19,14 +26,9 @@ button_login.addEventListener('click', (event) => {
   var storedUsername = localStorage.getItem('username')
   var storedPassword = localStorage.getItem('password')
 
-  console.log(typeof(usern.value))
-  console.log(passw.value)
-  console.log(storedUsername)
-  console.log(storedPassword)
-
   if(usern.value == storedUsername && passw.value == storedPassword) {
     alert('You are loged in')
-    window.location.href = "../../index.html"
+    window.location.href = "index.html"
   } else{
     alert('ERROR.')
   }
